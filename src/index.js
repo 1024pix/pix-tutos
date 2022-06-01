d65e60c18eee384c0eff3a2aa2cb528969177573
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-06-01 10:48:38 +0200

    feat: project initialisation

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const DIST = 'dist';
-
-(async () => {
-    await fs.promises.mkdir(DIST);
-    await fs.promises.writeFile(`${DIST}/index.html`, 'Hello, Tutos!', 'utf8')
-})();
