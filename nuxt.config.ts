d066be4efa78e837c6d84c5cc25e4e7ea6ecfb9f
Matthias FERRAINA <matthias.ferraina@pix.fr>
2022-06-07 11:41:43 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import { defineNuxtConfig } from "nuxt";
+import { Static } from "nuxt/dist/app/compat/capi";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
+  modules: ["@nuxt/content"],
 });
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,8 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc v-slot="{ doc }">
+      <h1>{{ doc.title }}</h1>
+      <p>{{ doc.description }}</p>
+    </ContentDoc>
   </main>
 </template>
