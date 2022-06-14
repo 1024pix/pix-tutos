90270ced4dbf24cf6e895b7dbd44d30cc2eff86e
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-06-14 18:12:43 +0200

    feat: create a PixHeader component to improve layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,6 @@
 <template>
   <div id="app" class="app-viewport">
-    <header class="main-header">
-      <div class="main-header__container">
-        <div class="main-header__logos">
-          <img
-            src="~/assets/images/marianne-logo.svg"
-            class="main-header__logo"
-            alt=""
-          />
-          <a
-            class="main-header__link"
-            href="https://pix.fr"
-            title="Site officiel Pix.fr"
-          >
-            <img
-              src="~/assets/images/pix-logo.svg"
-              class="main-header__logo"
-              alt="Pix"
-            />
-          </a>
-        </div>
-        <div v-if="!isConnected" class="main-header__actions">
-          <PixButtonLink
-            href="https://app.pix.fr/connexion"
-            background-color="transparent-light"
-          >
-            Se connecter
-          </PixButtonLink>
-          <PixButtonLink href="https://app.pix.fr/inscription">
-            S'inscrire
-          </PixButtonLink>
-        </div>
-      </div>
-    </header>
+    <PixHeader :is-connected="isConnected" />
     <main role="main" class="main-container">
       <nuxt />
     </main>
@@ -62,34 +30,6 @@ export default {
   min-height: 100vh;
   font-family: $open-sans;
   background-color: $background-light;
-}
-
-.main-header {
-  background-color: white;
-  box-shadow: $box-shadow-xs;
-}
-
-.main-header__container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  gap: 1.5rem;
-  padding: 1rem;
-  max-width: 1280px;
-  margin: 0 auto;
-
-  > * {
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 1.5rem;
-  }
-}
-
-.main-header__logo {
-  display: block;
-  height: 60px;
 }
 
 .main-container {
