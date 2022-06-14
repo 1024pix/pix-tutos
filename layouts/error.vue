1f46268e963971ddb6b017e819c2461d530af3d4
alexandrecoin <alexandre.pc.coin@gmail.com>
2022-06-14 15:17:18 +0200

    feat: add custom 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="error">
+    <a href="https://pix.fr/">
+      <img
+        class="error__logo"
+        src="~/assets/images/pix-logo.svg"
+        alt="Lien pour revenir à l'accueil"
+      />
+    </a>
+    <p class="error__message">{{ error.message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  head() {
+    return {
+      title: 'Erreur | Pix-tutos',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.error {
+  width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+
+  &__logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  &__message {
+    text-align: center;
+  }
+}
+</style>
