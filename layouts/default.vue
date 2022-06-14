c127e2bff02bf25c98349bc5ace4f81469cf8d99
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-06-14 15:48:38 +0200

    feat: add links to pix if user is not connected

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,22 +2,35 @@
   <div id="app" class="app-viewport">
     <header class="main-header">
       <div class="main-header__container">
-        <img
-          src="~/assets/images/marianne-logo.svg"
-          class="main-header__logo"
-          alt=""
-        />
-        <a
-          class="main-header__link"
-          href="https://pix.fr"
-          title="Site officiel Pix.fr"
-        >
+        <div class="main-header__logos">
           <img
-            src="~/assets/images/pix-logo.svg"
+            src="~/assets/images/marianne-logo.svg"
             class="main-header__logo"
-            alt="Pix"
+            alt=""
           />
-        </a>
+          <a
+            class="main-header__link"
+            href="https://pix.fr"
+            title="Site officiel Pix.fr"
+          >
+            <img
+              src="~/assets/images/pix-logo.svg"
+              class="main-header__logo"
+              alt="Pix"
+            />
+          </a>
+        </div>
+        <div v-if="!isConnected" class="main-header__actions">
+          <PixButtonLink
+            href="https://app.pix.fr/connexion"
+            background-color="transparent-light"
+          >
+            Se connecter
+          </PixButtonLink>
+          <PixButtonLink href="https://app.pix.fr/inscription">
+            S'inscrire
+          </PixButtonLink>
+        </div>
       </div>
     </header>
     <main role="main" class="main-container">
@@ -25,6 +38,24 @@
     </main>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isConnected: false,
+    };
+  },
+
+  // The `mounted` lifecycle hook is called client side
+  // https://nuxtjs.org/docs/concepts/nuxt-lifecycle/#client
+  mounted() {
+    if (location.search.includes('connecte')) {
+      this.isConnected = true;
+    }
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -41,18 +72,24 @@
 .main-header__container {
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 1.5rem;
   padding: 1rem;
   max-width: 1280px;
   margin: 0 auto;
+
+  > * {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 1.5rem;
+  }
 }
 
 .main-header__logo {
   display: block;
   height: 60px;
-}
-
-.main-header__link {
-  margin-left: 1.5rem;
 }
 
 .main-container {
