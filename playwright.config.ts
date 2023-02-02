8682c15aafb9681aea98de9bb5ee952f50d20507
Vincent Hardouin <vincent.hardouin@pix.fr>
2023-02-02 16:12:13 +0100

    chore: add playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -98,10 +98,10 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'PORT=3002 npm run dev',
+    port: 3002,
+  },
 };
 
 export default config;
