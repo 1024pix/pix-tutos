48e9a4f8b028c6a1b36e99df0b06239f3d4eabe6
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-05-12 14:24:07 +0200

    fix: matomo load

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
         },
         isSeoIndexingEnabled() ? {} : { name: 'robots', content: 'noindex' },
       ],
-      script: [],
+      script: [{ src: '/scripts/start-matomo-event.js' }],
     },
   },
 
