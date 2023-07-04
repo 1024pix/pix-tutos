8846c8d9d6e4d9350d46429a13263bdb596de4ea
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-07-04 15:23:39 +0200

    feat: add visual regression tests

---
+++ tests/e2e/visual.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('content', () => {
+  test('should show correctly', async ({ page }) => {
+    // given
+    const pageToVisit = '/edu'
+
+    // when
+    await page.goto(pageToVisit)
+
+    // then
+    await expect
+      .soft(page.getByRole('heading', { name: 'Tutoriels Réseau Canopé-Pix' }))
+      .toBeVisible()
+
+    await expect(page).toHaveScreenshot()
+  })
+})
