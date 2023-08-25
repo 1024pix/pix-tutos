5ee15219af2313f0ea159e405f5ac0a0d8f2b271
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-08-25 17:04:24 +0200

    feat: setup plyr and PixVideoPlayer component

---
+++ components/PixVideoPlayer.vue
@@ -0,0 +1,52 @@
+<script setup>
+import Plyr from 'plyr'
+
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  sourceMp4: {
+    type: String,
+    required: true,
+  },
+  captionVtt: {
+    type: String,
+    required: true,
+  },
+})
+
+const video = ref(null)
+onMounted(() => {
+  // eslint-disable-next-line no-new
+  new Plyr(video.value, { hideControls: false })
+})
+</script>
+
+<template>
+  <video
+    :id="id"
+    ref="video"
+    class="pix-video-player"
+    playsinline
+    controls
+  >
+    <source
+      :src="sourceMp4"
+      type="video/mp4"
+    >
+    <track
+      kind="captions"
+      label="Sous titres"
+      :src="captionVtt"
+      srclang="fr"
+      default
+    >
+  </video>
+</template>
+
+<style lang="scss">
+.pix-video-player {
+  width: 100%;
+}
+</style>
