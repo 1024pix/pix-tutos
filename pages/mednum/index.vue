37172277e1bafd81bfa9870a042c1af0166d18f2
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-09-29 14:23:14 +0200

    fix: run eslint

--- pages/mednum/index.vue
+++ pages/mednum/index.vue
@@ -11,7 +11,9 @@ const tutos = await queryContent('mednum').sort({ title: 1 }).find()
       Tutoriels produits par la MedNum pour Pix
     </h1>
 
-    <p class="pix-body-l header__description">Améliorez vos compétences sur la messagerie électronique, l'identité numérique, la datavisualisation et l'impact environnemental du numérique grâce à ces tutoriels vidéos ou infographies, produits par la MedNum pour Pix dans le cadre d'un projet Proof of Concept (POC).</p>
+    <p class="pix-body-l header__description">
+      Améliorez vos compétences sur la messagerie électronique, l'identité numérique, la datavisualisation et l'impact environnemental du numérique grâce à ces tutoriels vidéos ou infographies, produits par la MedNum pour Pix dans le cadre d'un projet Proof of Concept (POC).
+    </p>
 
     <section>
       <ul class="tutorial-list">
